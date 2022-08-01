aaede47b936167af48a265dc4702283b6eb9af2b
DinaraAminova <98523810+DinaraAminova@users.noreply.github.com>
2022-08-01 15:08:15 +0500

    Add files via upload

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostsList, PostDetailView
+from .views import PostsList, PostDetailView, SearchList
 
 
 urlpatterns = [
     path('', PostsList.as_view()),
     path('<int:pk>', PostDetailView.as_view()),
+    path('search', SearchList.as_view()),
 
 ]
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,8 +1,12 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.utils.datetime_safe import datetime
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, UpdateView, CreateView, DetailView
 from .models import Post, PostCategory, Comment
+from .filters import PostFilter
+from .forms import PostForm
 from datetime import datetime
+from django.core.paginator import Paginator
+
 # Create your views here.
 
 class PostsList(ListView):
@@ -10,6 +14,8 @@ class PostsList(ListView):
     template_name = 'news.html'
     context_object_name = 'news'
     queryset = Post.objects.order_by('-id')
+    ordering = ['-postCategory']
+    paginate_by = 1
 
 
 class PostDetailView(DetailView):
@@ -21,3 +27,15 @@ class PostDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['time_now'] = datetime.utcnow()
         return context
+
+class SearchList(ListView):
+    model = Post
+    template_name = 'search.html'
+    context_object_name = 'search'
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):  # забираем отфильтрованные объекты переопределяя метод get_context_data у наследуемого класса (привет, полиморфизм, мы скучали!!!)
+        context = super().get_context_data(**kwargs)
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())  # вписываем наш фильтр в контекст
+        return context
+
