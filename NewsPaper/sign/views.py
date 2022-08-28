9fc77796b6f50c052a105499c6720564b42cfe93
DinaraAminova <98523810+DinaraAminova@users.noreply.github.com>
2022-08-28 14:25:57 +0500

    Add files via upload

--- NewsPaper/sign/views.py
+++ NewsPaper/sign/views.py
@@ -1,1 +1,24 @@
+from django.shortcuts import render, redirect
 
+from django.contrib.auth.models import User, Group
+from django.views.generic.edit import CreateView
+from django.contrib.auth.decorators import login_required
+from .models import BaseRegisterForm
+
+
+@login_required
+def upgrade_me(request):
+    user = request.user
+    authors_group = Group.objects.get(name='authors')
+    if not request.user.groups.filter(name='authors').exists():
+        authors_group.user_set.add(user)
+    return redirect('/')
+
+class BaseRegisterView(CreateView):
+    model = User
+    form_class = BaseRegisterForm
+    success_url = '/'
+
+
+
+# Create your views here.
